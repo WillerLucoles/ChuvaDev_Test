d124d4dbbdbd8189fdbafd7ca4d36b3270e0bb87
Willer Lucoles <132530244+WillerLucoles@users.noreply.github.com>
2024-04-23 14:21:11 -0300

    Projeto Frontend finalizado

---
+++ ts/src/app/components/Apresentation/apresentation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-apresentation',
+    templateUrl: './apresentation.component.html',
+    styleUrls: ['./apresentation.component.scss']
+  })
+
+
+export class BoxApresentation {
+  }
